257073332000c029d6bed4ab5a278c5f2785f59d
Gunnar Gylfason <gunnargylfason@Gunnars-MacBook-Pro.local>
2023-12-30 00:45:57 +0000

    initial scaffolding

---
+++ modules/smart-object-framework/src/systems/SmartObjectSystem.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+import { System } from "@latticexyz/world/src/System.sol";
+// import { Tasks, TasksData } from "../codegen/index.sol";
+
+contract SmartObjectSystem is System {
+
+}
---
+++ modules/smart-storage-unit/mud.config.ts
@@ -0,0 +1,13 @@
+import { mudConfig } from "@latticexyz/world/register";
+
+export default mudConfig({
+  tables: {
+    Characters: {
+      valueSchema: {
+        createdAt: "uint256",
+        name: "string",
+        description: "string",
+      },
+    },
+  },
+});
