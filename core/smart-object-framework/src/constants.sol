4afa1794ef4a540e7f0f7d94a34b12c5f6bc9a2a
F0lks <julienk@ccpgames.com>
2024-03-06 12:37:17 +0000

    chore: diff cleanup with develop
    Git is *really* hard, dang it

--- core/smart-object-framework/src/constants.sol
+++ core/smart-object-framework/src/constants.sol
@@ -18,14 +18,6 @@ bytes14 constant SMART_OBJECT_MODULE_NAMESPACE = "SmartObjModule";
 // so it's best to just hard-code this for now
 bytes14 constant SMART_OBJECT_DEPLOYMENT_NAMESPACE = "SmartObject_v0";
 
-// TODO: this needs to match with the namespace used during `world.installModule(smartObjectModule, abi.encode("namespace"))`
-// and/or match the `namespace` field in mud.config.ts, depending how you deploy the Smart Object Framework 
-// (e.g. through `mud deploy` or inside a post-deployment Forge script) 
-// it's a bit of an inconvenience, but refactoring the namespace used to access the right tables in EveSystem dynamically would
-// require to read some form of storage, and that makes up for a lot of read operations and it's costly
-// so it's best to just hard-code this for now
-bytes14 constant SMART_OBJECT_DEPLOYMENT_NAMESPACE = "SmartObject_v0";
-
 ResourceId constant SMART_OBJECT_MODULE_NAMESPACE_ID = ResourceId.wrap(
   bytes32(abi.encodePacked(RESOURCE_NAMESPACE, SMART_OBJECT_MODULE_NAMESPACE))
 );
