dca34036e6d956fc79497d6674a33db57d8c7c49
gunnigylfa <gunnargylfa@gmail.com>
2024-03-11 16:24:50 +0100

    Make the lint fail not the build

--- modules/smart-storage-unit/src/systems/SmartStorageUnit.sol
+++ modules/smart-storage-unit/src/systems/SmartStorageUnit.sol
@@ -7,7 +7,6 @@ import { SmartStorageUnits, SmartStorageUnitsData } from "../codegen/index.sol";
 contract SmartStorageUnit is System {
   function create_smart_storage_unit(string memory name, string memory description) public returns (bytes32 key) {
     key = keccak256(abi.encode(block.prevrandao, _msgSender(), description));
-    throw;
 
     SmartStorageUnits.set(key, SmartStorageUnitsData({name: name, description: description, createdAt: block.timestamp}));
   }
