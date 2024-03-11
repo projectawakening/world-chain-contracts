a5391f26569a9ccf122a6e6a73b9a4a6c29163ec
gunnigylfa <gunnargylfa@gmail.com>
2024-03-11 15:58:46 +0100

    Make lint fail again

--- modules/smart-storage-unit/src/systems/SmartStorageUnit.sol
+++ modules/smart-storage-unit/src/systems/SmartStorageUnit.sol
@@ -5,8 +5,7 @@ import { System } from "@latticexyz/world/src/System.sol";
 import { SmartStorageUnits, SmartStorageUnitsData } from "../codegen/index.sol";
 
 contract SmartStorageUnit is System {
-
-  function createSmartStorageUnit(string memory name, string memory description) public returns (bytes32 key) {
+  function create_smart_storage_unit(string memory name, string memory description) public returns (bytes32 key) {
     key = keccak256(abi.encode(block.prevrandao, _msgSender(), description));
     throw;
 
