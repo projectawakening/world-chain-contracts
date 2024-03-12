76af9799a55f4e623391a73bc6ec91c0684a89a3
CCP Legolas <toniya@ccpgames.com>
2024-03-12 23:25:09 +0000

    data store for smart deployables

--- modules/smart-storage-unit/mud.config.ts
+++ modules/smart-storage-unit/mud.config.ts
@@ -7,12 +7,52 @@ export default mudConfig({
       openAccess: true,
     },
   },
+  enums: {
+    State: ["ANCHOR", "UNANCHOR", "ONLINE", "OFFLINE", "DESTROYED"],
+  },
   tables: {
-    SmartStorageUnits: {
+    EntityRecord: {
+      keySchema: {
+        entityId: "uint256",
+      },
+      valueSchema: {
+        itemId: "uint256",
+        typeId: "uint256",
+        volume: "uint256",
+      },
+    },
+    Location: {
+      keySchema: {
+        smartObjectId: "uint256",
+      },
+      valueSchema: {
+        solarsystemId: "uint256",
+        x: "uint256",
+        y: "uint256",
+        z: "uint256",
+      },
+    },
+    DeployableState: {
+      keySchema: {
+        smartObjectId: "uint256",
+      },
       valueSchema: {
         createdAt: "uint256",
-        name: "string",
-        description: "string",
+        state: "State",
+        updatedBlockNumber: "uint256",
+      },
+    },
+    StaticDataGlobal: {
+      valueSchema: {
+        baseURI: "bytes32",
+      },
+    },
+    StaticData: {
+      keySchema: {
+        smartObjectId: "uint256",
+      },
+      valueSchema: {
+        cid: "bytes32",
       },
     },
   },
