86d42509eb8961005ec729d3a228977a6367a127
CCP Legolas <toniya@ccpgames.com>
2024-03-13 15:47:48 +0000

    reducing the string length to comply with the byte size

--- modules/smart-storage-unit/src/systems/constants.sol
+++ modules/smart-storage-unit/src/systems/constants.sol
@@ -4,5 +4,5 @@ pragma solidity >=0.8.21;
 import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
 import { RESOURCE_NAMESPACE } from "@latticexyz/world/src/worldResourceTypes.sol";
 
-bytes16 constant SMART_STORAGE_MODULE_NAME = "SmartStorageUnit";
-bytes14 constant SMART_STORAGE_MODULE_NAMESPACE = "SmartStorageUnit";
+bytes16 constant SMART_STORAGE_MODULE_NAME = "SmartStorage";
+bytes14 constant SMART_STORAGE_MODULE_NAMESPACE = "SmartStorage";
