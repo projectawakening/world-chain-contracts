f5e1e7515ca4839d53830d24be8fb756204885a5
CCP Legolas <toniya@ccpgames.com>
2024-03-14 11:10:59 +0000

    creating a sample script to test execution

--- modules/smart-character/mud.config.ts
+++ modules/smart-character/mud.config.ts
@@ -1,6 +1,12 @@
 import { mudConfig } from "@latticexyz/world/register";
 
 export default mudConfig({
+  systems: {
+    SmartCharacterSystem: {
+      name: "SmartCharacterSystem",
+      openAccess: true,
+    },
+  },
   tables: {
     /**
      * Maps the in-game character ID to on-chain EOA address
