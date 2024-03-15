ae4d06908de12d8bd255f9a03565893f12e53f6c
skygirl <100742846+0xxlegolas@users.noreply.github.com>
2024-03-15 09:26:36 +0000

    feat: data store and interface definitions  (#21)
    * data store for smart deployables
    
    * interface definition for the smart deployable
    
    * interface and datastore for inventory
    
    * adding smart character interface
    
    * fixing typo
    
    * reducing the string length to comply with the byte size
    
    * - adding global state for deployables
    - making EntityRecordMetadata as offchain table
    
    * creating a sample script to test execution
    
    * adding ERC721 puppet module installation as part of the deploy script to see the ERC721 tables in the indexer
    
    * adding env file
    
    * added owner as a composite key to EphemeralInventory table
    
    * added systemId as a key  for StaticDataGlobal

---
+++ modules/smart-storage-unit/src/systems/constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
+import { RESOURCE_NAMESPACE } from "@latticexyz/world/src/worldResourceTypes.sol";
+
+bytes16 constant SMART_STORAGE_MODULE_NAME = "SmartStorage";
+bytes14 constant SMART_STORAGE_MODULE_NAMESPACE = "SmartStorage";
