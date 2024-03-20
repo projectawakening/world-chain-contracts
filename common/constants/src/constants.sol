645adad5d699755eb41ef9075e9da76165e95893
F0lks <36047278+MerkleBoy@users.noreply.github.com>
2024-03-20 13:38:49 +0000

    change all deployment namespaces to `frontier`

--- common/constants/src/constants.sol
+++ common/constants/src/constants.sol
@@ -3,9 +3,9 @@ pragma solidity >=0.8.21;
 
 // make sure this matches constants.ts
 
-bytes14 constant SMART_OBJECT_DEPLOYMENT_NAMESPACE = "SmartObject_v0";
-bytes14 constant ACCESS_CONTROL_DEPLOYMENT_NAMESPACE = "RBAC_v0";
-bytes14 constant ENTITY_RECORD_DEPLOYMENT_NAMESPACE = "EntityRecor_v0";
-bytes14 constant STATIC_DATA_DEPLOYMENT_NAMESPACE = "StaticData_v0";
-bytes14 constant SMART_CHARACTER_DEPLOYMENT_NAMESPACE = "SmartChar_v0";
-bytes14 constant EVE_ERC721_PUPPET_DEPLOYMENT_NAMESPACE = "EveERC721_v0";
+bytes14 constant SMART_OBJECT_DEPLOYMENT_NAMESPACE = "frontier";
+bytes14 constant ACCESS_CONTROL_DEPLOYMENT_NAMESPACE = "frontier";
+bytes14 constant ENTITY_RECORD_DEPLOYMENT_NAMESPACE = "frontier";
+bytes14 constant STATIC_DATA_DEPLOYMENT_NAMESPACE = "frontier";
+bytes14 constant SMART_CHARACTER_DEPLOYMENT_NAMESPACE = "frontier";
+bytes14 constant EVE_ERC721_PUPPET_DEPLOYMENT_NAMESPACE = "frontier";
