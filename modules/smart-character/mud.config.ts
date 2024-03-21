cc16c923a181c068711673871e7d6bc21973da5a
F0lks <36047278+MerkleBoy@users.noreply.github.com>
2024-03-21 21:08:33 +0000

    temp fix: mudconfig error
    need to sync with Gunnar

--- modules/smart-character/mud.config.ts
+++ modules/smart-character/mud.config.ts
@@ -1,4 +1,35 @@
 import { mudConfig } from "@latticexyz/world/register";
-import config from "./mudConfigObject.ts";
+import constants from "@eve/common-constants/src/constants.json" assert { type: "json" };
 
-export default mudConfig(config);
+export default mudConfig({
+  namespace: constants.SMART_CHARACTER_DEPLOYMENT_NAMESPACE,
+  systems: {
+    SmartCharacter: {
+      name: "SmartCharacter",
+      openAccess: true,
+    },
+  },
+  tables: {
+    /**
+     * Maps the in-game character ID to on-chain EOA address
+     */
+    CharactersTable: {
+      keySchema: {
+        characterId: "uint256",
+      },
+      valueSchema: {
+        characterAddress: "address",
+        createdAt: "uint256"
+      },
+      tableIdArgument: true,
+    },
+    
+    CharactersConstantsTable: {
+      keySchema: {},
+      valueSchema: {
+        erc721Address: "address",
+      },
+      tableIdArgument: true,
+    },
+  },
+});
