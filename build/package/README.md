7c15467fa7f5a11762a71a2ae35f4bf27a7fe012
Gunnar Gylfason <gunnargylfa@gmail.com>
2024-04-12 17:12:52 +0200

    Initial commit for building image and adding documentation

---
+++ build/package/README.md
@@ -0,0 +1,22 @@
+# Frontier World deployer image
+
+This image contains the packaged source code required to build, test and deploy the Frontier world as a whole.
+
+# Installation 
+Pull the image from our docker registry (URL TBD).
+
+and run it with the command: 
+
+```bash
+ docker run --name frontier-world-deployer -it frontier-image  --rpc-url http://host.docker.internal:8545 --private-key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
+```
+
+Make sure to have a running EVM node available and pass the appropriate url as a parameter. If you are running the node locally on a Mac or Windows you will need to reference it with the `host.docker.internal` host. On Linux machines you can use `--net=host` as a parameter instead.
+
+To ABIs from this deployer image you can run: 
+
+```bash 
+docker cp frontier-deployer-container:/monorepo/abis .
+```
+This copies the directory containing the ABIs from the deployment into your current directory.
---
+++ build/package/deploy-in-docker.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+
+
+bar_size=40
+bar_char_done="#"
+bar_char_todo="-"
+bar_percentage_scale=2
+
+
+show_progress() {
+    current="$1"
+    total="$2"
+
+    # calculate the progress in percentage using awk for floating point arithmetic with fixed precision
+    percent=$(awk -v current="$current" -v total="$total" \
+        'BEGIN {printf "%.2f", (100 * current / total)}')
+
+    # Calculate the number of done and todo characters using awk
+    done=$(awk -v percent="$percent" -v bar_size="$bar_size" 'BEGIN {printf "%d", int(bar_size * percent / 100)}')
+    todo=$(awk -v done="$done" -v bar_size="$bar_size" 'BEGIN {printf "%d", int(bar_size - done)}')
+
+    # Build the done and todo sub-bars
+    done_sub_bar=$(printf "%${done}s" | tr " " "$bar_char_done")
+    todo_sub_bar=$(printf "%${todo}s" | tr " " "$bar_char_todo")
+
+    # Output the bar
+    printf "\rProgress : [${done_sub_bar}${todo_sub_bar}] ${percent}%%"
+
+    if [ "$total" -eq "$current" ]; then
+        printf "\nSuccess: Frontier world deployed\n"
+    fi
+}
+
+
+# Default values
+rpc_url=""
+private_key=""
+
+# Parse command-line arguments
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -p1|--rpc-url)
+            rpc_url="$2"
+            shift 2
+            ;;
+        -p2|--private-key)
+            private_key="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+## Temporarily hardcode private key and rpc url before adding them as params
+export RPC_URL="$rpc_url"
+export PRIVATE_KEY="$private_key"
+
+
+show_progress 0 6
+
+
+#1 Deploying the standard contracts
+echo " - Deploying standard contracts..."
+pnpm nx run @eve/frontier-standard-contracts:deploy &> '/dev/null'
+wait
+show_progress 1 6
+
+export FORWARDER_ADDRESS=$(cat ./standard-contracts/broadcast/Deploy.s.sol/31337/run-latest.json | jq '.transactions|first|.contractAddress' | tr -d \") 
+
+#2 Deploy the world core
+echo " - Deploying frontier world..."
+pnpm nx deploy @eve/frontier-world-core &> '/dev/null'
+wait
+show_progress 2 6
+
+export WORLD_ADDRESS=$(cat ./mud-contracts/core/deploys/31337/latest.json | jq '.worldAddress' | tr -d \")
+
+#3 Configure the world to receive the forwarder
+echo " - Configuring trusted forwarder within the world"
+pnpm nx setForwarder @eve/frontier-world-core &> '/dev/null'
+
+wait
+show_progress 3 6
+
+
+#4 Deploy smart object framework 
+#
+# TODO stop using :local for all the 
+echo " - Installing smart object framework into world"
+pnpm nx deploy @eve/frontier-smart-object-framework --worldAddress '${WORLD_ADDRESS}' &> '/dev/null'
+show_progress 4 6
+
+#5 Deploy Frontier world features
+echo " - Deploying world features"
+pnpm nx deploy @eve/frontier-world --worldAddress '${WORLD_ADDRESS}' &> '/dev/null'
+show_progress 5 6
+
+echo " - Collecting ABIs"
+mkdir abis
+mkdir apis/trusted-forwarder
+mkdir apis/frontier-world
+
+cp -r standard-contracts/out/ERC2771ForwarderWithHashNonce.sol/ abis/trusted-forwarder
+cp -r mud-contracts/frontier-world/out/IWorld.sol/ abis/frontier-world
+show_progress 6 6
+
+
+echo "Frontier world successfully deployed"
+echo "World address: $WORLD_ADDRESS"
+echo "Trusted forwarder address: $FORWARDER_ADDRESS" 
