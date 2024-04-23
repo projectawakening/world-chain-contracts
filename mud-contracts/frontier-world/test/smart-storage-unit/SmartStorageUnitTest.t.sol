b49ae9b3cbb6b7a8bc373bece5947213d7135680
CCP Legolas <toniya@ccpgames.com>
2024-04-23 11:11:19 +0000

    adding template for tests

--- mud-contracts/frontier-world/test/smart-storage-unit/SmartStorageUnitTest.t.sol
+++ mud-contracts/frontier-world/test/smart-storage-unit/SmartStorageUnitTest.t.sol
@@ -13,7 +13,7 @@ import { PuppetModule } from "@latticexyz/world-modules/src/modules/puppet/Puppe
 
 import { SMART_OBJECT_DEPLOYMENT_NAMESPACE } from "@eve/common-constants/src/constants.sol";
 import { SmartObjectFrameworkModule } from "@eve/frontier-smart-object-framework/src/SmartObjectFrameworkModule.sol";
-import { SMART_STORAGE_UNIT_DEPLOYMENT_NAMESPACE } from "@eve/common-constants/src/constants.sol";
+import { EVE_ERC721_PUPPET_DEPLOYMENT_NAMESPACE, STATIC_DATA_DEPLOYMENT_NAMESPACE, SMART_STORAGE_UNIT_DEPLOYMENT_NAMESPACE, ENTITY_RECORD_DEPLOYMENT_NAMESPACE, SMART_DEPLOYABLE_DEPLOYMENT_NAMESPACE, INVENTORY_DEPLOYMENT_NAMESPACE } from "@eve/common-constants/src/constants.sol";
 
 import { StaticDataModule } from "../../src/modules/static-data/StaticDataModule.sol";
 import { EntityRecordModule } from "../../src/modules/entity-record/EntityRecordModule.sol";
@@ -23,6 +23,7 @@ import { IERC721Mintable } from "../../src/modules/eve-erc721-puppet/IERC721Mint
 import { IERC721Metadata } from "../../src/modules/eve-erc721-puppet/IERC721Metadata.sol";
 import { SmartStorageUnitModule } from "../../src/modules/smart-storage-unit/SmartStorageUnitModule.sol";
 import { SmartDeployableModule } from "../../src/modules/smart-deployable/SmartDeployableModule.sol";
+import { InventoryModule } from "../../src/modules/inventory/InventoryModule.sol";
 
 import { Utils as SmartStorageUnitUtils } from "../../src/modules/smart-storage-unit/Utils.sol";
 import { Utils as EntityRecordUtils } from "../../src/modules/entity-record/Utils.sol";
@@ -30,13 +31,13 @@ import { Utils as SmartDeployableUtils } from "../../src/modules/smart-deployabl
 
 import { SmartStorageUnitLib } from "../../src/modules/smart-storage-unit/SmartStorageUnitLib.sol";
 import { StaticDataGlobalTableData } from "../../src/codegen/tables/StaticDataGlobalTable.sol";
-import { EntityRecordTable, EntityRecordTableData } from "../../src/codegen/tables/EntityRecordTable.sol";
-import { EntityRecordOffchainTableData } from "../../src/codegen/tables/EntityRecordOffchainTable.sol";
+import "../../src/modules/smart-storage-unit/types.sol";
 import { createCoreModule } from "../CreateCoreModule.sol";
 
 contract SmartStorageUnitTest is Test {
   using SmartStorageUnitUtils for bytes14;
   using EntityRecordUtils for bytes14;
+  using SmartStorageUnitLib for SmartStorageUnitLib.World;
   using WorldResourceIdInstance for ResourceId;
 
   IBaseWorld baseWorld;
@@ -50,13 +51,14 @@ contract SmartStorageUnitTest is Test {
 
     // install module dependancies
     baseWorld.installModule(new PuppetModule(), new bytes(0));
-    baseWorld.installModule(new StaticDataModule(), abi.encode(SMART_STORAGE_UNIT_DEPLOYMENT_NAMESPACE));
-    baseWorld.installModule(new EntityRecordModule(), abi.encode(SMART_STORAGE_UNIT_DEPLOYMENT_NAMESPACE));
-    baseWorld.installModule(new SmartDeployableModule(), abi.encode(SMART_STORAGE_UNIT_DEPLOYMENT_NAMESPACE));
+    baseWorld.installModule(new StaticDataModule(), abi.encode(STATIC_DATA_DEPLOYMENT_NAMESPACE));
+    baseWorld.installModule(new EntityRecordModule(), abi.encode(ENTITY_RECORD_DEPLOYMENT_NAMESPACE));
+    baseWorld.installModule(new SmartDeployableModule(), abi.encode(SMART_DEPLOYABLE_DEPLOYMENT_NAMESPACE));
+    baseWorld.installModule(new InventoryModule(), abi.encode(INVENTORY_DEPLOYMENT_NAMESPACE));
     StoreSwitch.setStoreAddress(address(baseWorld));
     erc721Token = registerERC721(
       baseWorld,
-      SMART_STORAGE_UNIT_DEPLOYMENT_NAMESPACE,
+      EVE_ERC721_PUPPET_DEPLOYMENT_NAMESPACE,
       StaticDataGlobalTableData({ name: "SmartStorageUnit", symbol: "SSU", baseURI: "" })
     );
 
@@ -70,5 +72,51 @@ contract SmartStorageUnitTest is Test {
     );
     ResourceId smartStorageUnitSystemId = SystemRegistry.get(smartStorageUnitSystem);
     assertEq(smartStorageUnitSystemId.getNamespace(), SMART_STORAGE_UNIT_DEPLOYMENT_NAMESPACE);
+  }
+
+  function testCreateAndAnchorSmartStorageUnit(uint256 smartObjectId) public {
+    uint256 storageCapacity = 100000;
+    uint256 ephemeralStorageCapacity = 100000;
+
+    EntityRecordData memory entityRecordData = EntityRecordData({ typeId: 12345, itemId: 45, volume: 10 });
+    SmartObjectData memory smartObjectData = SmartObjectData({ owner: address(0), tokenURI: "test" });
+    WorldPosition memory worldPosition = WorldPosition({ solarSystemId: 1, position: Coord({ x: 1, y: 1, z: 1 }) });
+    smartStorageUnit.createAndAnchorSmartStorageUnit(
+      smartObjectId,
+      entityRecordData,
+      smartObjectData,
+      worldPosition,
+      storageCapacity,
+      ephemeralStorageCapacity
+    );
+  }
+
+  function testCreateAndDepositItemsToInventory(uint256 smartObjectId) public {
+    InventoryItem[] memory items = new InventoryItem[](1);
+    items[0] = InventoryItem({
+      inventoryItemId: 123,
+      owner: address(0),
+      itemId: 12,
+      typeId: 3,
+      volume: 10,
+      quantity: 5
+    });
+
+    smartStorageUnit.createAndDepositItemsToInventory(smartObjectId, items);
+  }
+
+  function testCreateAndDepositItemsToEphemeralInventory(uint256 smartObjectId) public {
+    InventoryItem[] memory items = new InventoryItem[](1);
+    address inventoryOwner = address(0);
+    items[0] = InventoryItem({
+      inventoryItemId: 456,
+      owner: address(1),
+      itemId: 45,
+      typeId: 6,
+      volume: 10,
+      quantity: 5
+    });
+
+    smartStorageUnit.createAndDepositItemsToEphemeralInventory(smartObjectId, inventoryOwner, items);
   }
 }
