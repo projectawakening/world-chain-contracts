b050ba66160dd4dae0d13d653cb41f932c21e9d5
CCP Legolas <toniya@ccpgames.com>
2024-04-30 04:05:56 +0000

    adding scripts for testing

---
+++ end-to-end-tests/script/SetDeployableMetadata.s.sol
@@ -0,0 +1,32 @@
+pragma solidity >=0.8.20;
+
+import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+import { ResourceId, WorldResourceIdLib } from "@latticexyz/world/src/WorldResourceId.sol";
+import { IBaseWorld } from "@eve/frontier-world/src/codegen/world/IWorld.sol";
+import { SmartStorageUnitLib } from "@eve/frontier-world/src/modules/smart-storage-unit/SmartStorageUnitLib.sol";
+
+contract SetDeployableMetadata is Script {
+  using SmartStorageUnitLib for SmartStorageUnitLib.World;
+
+  function run(address worldAddress) public {
+    StoreSwitch.setStoreAddress(worldAddress);
+    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    address player = vm.addr(deployerPrivateKey);
+
+    // Start broadcasting transactions from the deployer account
+    vm.startBroadcast(deployerPrivateKey);
+    SmartStorageUnitLib.World memory smartStorageUnit = SmartStorageUnitLib.World({
+      iface: IBaseWorld(worldAddress),
+      namespace: "frontier"
+    });
+
+    uint256 smartObjectId = uint256(keccak256(abi.encode("item:<tenant_id>-<db_id>-2345")));
+
+    smartStorageUnit.setDeploybaleMetadata(smartObjectId, "name", "dappURL", "description");
+
+    vm.stopBroadcast();
+  }
+}
