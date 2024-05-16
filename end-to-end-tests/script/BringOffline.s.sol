ca23929602fc4b6eab8a74a1d22d28d0fc9f6fe6
ccp-raudur <148865905+ccp-raudur@users.noreply.github.com>
2024-05-16 12:06:44 +0000

    Fixing wrong .env being imported and adding end to end scripts (#75)

---
+++ end-to-end-tests/script/BringOffline.s.sol
@@ -0,0 +1,31 @@
+pragma solidity >=0.8.20;
+
+import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+import { ResourceId, WorldResourceIdLib } from "@latticexyz/world/src/WorldResourceId.sol";
+import { IBaseWorld } from "@eve/frontier-world/src/codegen/world/IWorld.sol";
+import { SmartDeployableLib } from "@eve/frontier-world/src/modules/smart-deployable/SmartDeployableLib.sol";
+import { FRONTIER_WORLD_DEPLOYMENT_NAMESPACE } from "@eve/common-constants/src/constants.sol";
+
+contract BringOffline is Script {
+  using SmartDeployableLib for SmartDeployableLib.World;
+
+  function run(address worldAddress) public {
+    StoreSwitch.setStoreAddress(worldAddress);
+    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+    // Start broadcasting transactions from the deployer account
+    vm.startBroadcast(deployerPrivateKey);
+    SmartDeployableLib.World memory smartDeployable = SmartDeployableLib.World({
+      iface: IBaseWorld(worldAddress),
+      namespace: FRONTIER_WORLD_DEPLOYMENT_NAMESPACE
+    });
+
+    uint256 smartObjectId = uint256(63172679203090930493472462170959929237896419587292096226487443413731496352045);
+    smartDeployable.bringOffline(smartObjectId);
+
+    vm.stopBroadcast();
+  }
+}
