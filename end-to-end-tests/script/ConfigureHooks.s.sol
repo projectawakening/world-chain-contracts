26eeeb02f5da24ea909a6a8400aee0443ef54178
CCP Legolas <toniya@ccpgames.com>
2024-05-20 23:22:29 +0000

    add configreHooksScript

--- end-to-end-tests/script/ConfigureHooks.s.sol
+++ end-to-end-tests/script/ConfigureHooks.s.sol
@@ -11,8 +11,10 @@ import { AccessControlSystem } from "@eveworld/world-core/src/systems/access-con
 import { Utils as AccessControlUtils } from "@eveworld/world-core/src/systems/access-control/Utils.sol";
 import { Role } from "@eveworld/world-core/src/codegen/tables/Role.sol";
 
+import { ISmartStorageUnit } from "@eveworld/world/src/modules/smart-storage-unit/interfaces/ISmartStorageUnit.sol";
 import { ISmartCharacter } from "@eveworld/world/src/modules/smart-character/interfaces/ISmartCharacter.sol";
 import { Utils as SmartCharacterUtils } from "@eveworld/world/src/modules/smart-character/Utils.sol";
+import { Utils as SmartStorageUnitUtils } from "@eveworld/world/src/modules/smart-storage-unit/Utils.sol";
 
 import { HookTable } from "@eveworld/smart-object-framework/src/codegen/tables/HookTable.sol";
 import { SmartObjectLib } from "@eveworld/smart-object-framework/src/SmartObjectLib.sol";
@@ -24,9 +26,12 @@ import { FRONTIER_WORLD_DEPLOYMENT_NAMESPACE } from "@eveworld/common-constants/
 
 contract ConfigureHooks is Script {
   using Utils for bytes14;
+  using SmartStorageUnitUtils for bytes14;
   using AccessControlUtils for bytes14;
   using SmartCharacterUtils for bytes14;
   using SmartObjectLib for SmartObjectLib.World;
+
+  SmartObjectLib.World smartObjectFramework;
 
   function run(address worldAddress) external {
     StoreSwitch.setStoreAddress(worldAddress);
@@ -43,26 +48,50 @@ contract ConfigureHooks is Script {
     console.log("Admin: ", admin);
 
     //Register Hook
-    SmartObjectLib.World memory smartObjectFramework = SmartObjectLib.World({
+    smartObjectFramework = SmartObjectLib.World({
       iface: IBaseWorld(worldAddress),
       namespace: FRONTIER_WORLD_DEPLOYMENT_NAMESPACE
     });
 
-    //Register Hook
+    //Register Hook onlyAdmin
     bytes4 onlyAdminHookFunctionId = IAccessControlSystem.onlyAdminHook.selector;
-    // smartObjectFramework.registerHook(
-    //   FRONTIER_WORLD_DEPLOYMENT_NAMESPACE.accessControlSystemId(),
-    //   onlyAdminHookFunctionId
-    // );
+    smartObjectFramework.registerHook(AccessControlUtils.accessControlSystemId(), onlyAdminHookFunctionId);
 
-    uint256 hookId = uint256(
-      keccak256(abi.encodePacked(FRONTIER_WORLD_DEPLOYMENT_NAMESPACE.accessControlSystemId(), onlyAdminHookFunctionId))
+    uint256 onlyAdminHookId = uint256(
+      keccak256(abi.encodePacked(AccessControlUtils.accessControlSystemId(), onlyAdminHookFunctionId))
     );
-    console.log("Hook ID: ", hookId);
+    console.log("Only Admin Hook ID: ", onlyAdminHookId);
 
-    // uint256 smartCharacterClassId = uint256(keccak256("SmartCharacterClass"));
-    // //asscoaite hook with a entity
-    // smartObjectFramework.associateHook(smartCharacterClassId, hookId);
+    //Register hook for onlyOwner
+    bytes4 onlyOwnerHookFunctionId = IAccessControlSystem.onlyOwnerHook.selector;
+
+    smartObjectFramework.registerHook(AccessControlUtils.accessControlSystemId(), onlyOwnerHookFunctionId);
+
+    uint256 onlyOwnerHookId = uint256(
+      keccak256(abi.encodePacked(AccessControlUtils.accessControlSystemId(), onlyOwnerHookFunctionId))
+    );
+    console.log("Only Owner Hook ID: ", onlyOwnerHookId);
+
+    //Add hook for createCharacter
+    _addHookForCreateSmartCharacter(smartObjectFramework, onlyAdminHookId);
+    _addHookForCreateAndAnchor(smartObjectFramework, onlyAdminHookId);
+
+    uint256 targetid = uint256(
+      keccak256(
+        abi.encodePacked(
+          FRONTIER_WORLD_DEPLOYMENT_NAMESPACE.smartCharacterSystemId(),
+          ISmartCharacter.createCharacter.selector
+        )
+      )
+    );
+
+    vm.stopBroadcast();
+  }
+
+  function _addHookForCreateSmartCharacter(SmartObjectLib.World memory smartObjectFramework, uint256 hookId) internal {
+    uint256 smartCharacterClassId = uint256(keccak256("SmartCharacterClass"));
+    //asscoaite hook with a entity
+    smartObjectFramework.associateHook(smartCharacterClassId, hookId);
 
     //Add hook for createCharacter
     smartObjectFramework.addHook(
@@ -71,7 +100,19 @@ contract ConfigureHooks is Script {
       FRONTIER_WORLD_DEPLOYMENT_NAMESPACE.smartCharacterSystemId(),
       ISmartCharacter.createCharacter.selector
     );
+  }
 
-    vm.stopBroadcast();
+  function _addHookForCreateAndAnchor(SmartObjectLib.World memory smartObjectFramework, uint256 hookId) internal {
+    uint256 ssuClassId = uint256(keccak256("SSUClass"));
+    //asscoaite hook with a entity
+    smartObjectFramework.associateHook(ssuClassId, hookId);
+
+    //Add hook for createCharacter
+    smartObjectFramework.addHook(
+      hookId,
+      HookType.AFTER,
+      FRONTIER_WORLD_DEPLOYMENT_NAMESPACE.smartStorageUnitSystemId(),
+      ISmartStorageUnit.createAndAnchorSmartStorageUnit.selector
+    );
   }
 }
