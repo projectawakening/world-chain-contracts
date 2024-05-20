33546f1d09ed32b2d60c0a69228952137ba32764
F0lks <36047278+MerkleBoy@users.noreply.github.com>
2024-05-20 16:31:43 +0000

    adding end to end test

---
+++ end-to-end-tests/script/CreateAndAnchorGateKeeper.s.sol
@@ -0,0 +1,49 @@
+pragma solidity >=0.8.20;
+
+import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+import { ResourceId, WorldResourceIdLib } from "@latticexyz/world/src/WorldResourceId.sol";
+import { IBaseWorld } from "@eveworld/world/src/codegen/world/IWorld.sol";
+import { EntityRecordData, SmartObjectData, WorldPosition, Coord } from "@eveworld/world/src/modules/smart-storage-unit/types.sol";
+import { GateKeeperLib } from "@eveworld/world/src/modules/gate-keeper/GateKeeperLib.sol";
+import { FRONTIER_WORLD_DEPLOYMENT_NAMESPACE } from "@eveworld/common-constants/src/constants.sol";
+
+contract CreateAndAnchor is Script {
+  using GateKeeperLib for GateKeeperLib.World;
+
+  function run(address worldAddress) public {
+    StoreSwitch.setStoreAddress(worldAddress);
+    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    address player = vm.addr(deployerPrivateKey);
+
+    // Start broadcasting transactions from the deployer account
+    vm.startBroadcast(deployerPrivateKey);
+    GateKeeperLib.World memory gateKeeper = GateKeeperLib.World({
+      iface: IBaseWorld(worldAddress),
+      namespace: FRONTIER_WORLD_DEPLOYMENT_NAMESPACE
+    });
+
+    uint256 smartObjectId = uint256(keccak256(abi.encode("item:<tenant_id>-<db_id>-2346")));
+    // uint256 storageCapacity = 100000000;
+    // uint256 ephemeralStorageCapacity = 100000000000;
+    // EntityRecordData memory entityRecordData = EntityRecordData({ typeId: 7888, itemId: 111, volume: 10 });
+    // SmartObjectData memory smartObjectData = SmartObjectData({ owner: player, tokenURI: "test" });
+    // WorldPosition memory worldPosition = WorldPosition({ solarSystemId: 1, position: Coord({ x: 1, y: 1, z: 1 }) });
+
+    gateKeeper.createAndAnchorGateKeeper(
+      smartObjectId,
+      EntityRecordData({ typeId: 7888, itemId: 111, volume: 10 }),
+      SmartObjectData({ owner: player, tokenURI: "test" }),
+      WorldPosition({ solarSystemId: 1, position: Coord({ x: 1, y: 1, z: 1 }) }),
+      1e18, // fuelUnitVolume,
+      1, // fuelConsumptionPerMinute,
+      1000000 * 1e18, //fuelMaxCapacity,
+      100000000, // storageCapacity,
+      100000000000 // ephemeralStorageCapacity
+    );
+
+    vm.stopBroadcast();
+  }
+}
---
+++ end-to-end-tests/script/GateKeeperConfig.s.sol
@@ -0,0 +1,34 @@
+pragma solidity >=0.8.20;
+
+import { Script } from "forge-std/Script.sol";
+import { console } from "forge-std/console.sol";
+import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
+import { ResourceId, WorldResourceIdLib } from "@latticexyz/world/src/WorldResourceId.sol";
+import { IBaseWorld } from "@eveworld/world/src/codegen/world/IWorld.sol";
+import { EntityRecordData, SmartObjectData, WorldPosition, Coord } from "@eveworld/world/src/modules/smart-storage-unit/types.sol";
+import { GateKeeperLib } from "@eveworld/world/src/modules/gate-keeper/GateKeeperLib.sol";
+import { FRONTIER_WORLD_DEPLOYMENT_NAMESPACE } from "@eveworld/common-constants/src/constants.sol";
+
+contract GateKeeperConfig is Script {
+  using GateKeeperLib for GateKeeperLib.World;
+
+  function run(address worldAddress) public {
+    StoreSwitch.setStoreAddress(worldAddress);
+    // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+    // Start broadcasting transactions from the deployer account
+    vm.startBroadcast(deployerPrivateKey);
+    GateKeeperLib.World memory gateKeeper = GateKeeperLib.World({
+      iface: IBaseWorld(worldAddress),
+      namespace: FRONTIER_WORLD_DEPLOYMENT_NAMESPACE
+    });
+
+    uint256 smartObjectId = uint256(keccak256(abi.encode("item:<tenant_id>-<db_id>-2346")));
+
+    gateKeeper.setAcceptedItemTypeId(smartObjectId, 0);
+    gateKeeper.setTargetQuantity(smartObjectId, 1000);
+
+    vm.stopBroadcast();
+  }
+}
