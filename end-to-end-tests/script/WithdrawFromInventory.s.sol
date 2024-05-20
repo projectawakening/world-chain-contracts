9e3ca5e4d6d4b4f09daf63ef9b07ac24f0322f99
skygirl <100742846+0xxlegolas@users.noreply.github.com>
2024-05-20 15:43:29 +0000

    withdraw bug fix (#84)
    * updating npm version
    
    * item index logic fixed
    
    * item index fix in ephemeral table
    
    * adding tests
    
    * adding scripts to test withdrawals
    
    * fix linting
    
    * withdraw logic fix by index update
    
    * fixing index logic in ephemeral inventory
    
    * remove comments

--- end-to-end-tests/script/WithdrawFromInventory.s.sol
+++ end-to-end-tests/script/WithdrawFromInventory.s.sol
@@ -6,11 +6,15 @@ import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 import { ResourceId, WorldResourceIdLib } from "@latticexyz/world/src/WorldResourceId.sol";
 import { IBaseWorld } from "@eveworld/world/src/codegen/world/IWorld.sol";
 import { InventoryItem } from "@eveworld/world/src/modules/inventory/types.sol";
+import { InventoryLib } from "@eveworld/world/src/modules/inventory/InventoryLib.sol";
 import { SmartStorageUnitLib } from "@eveworld/world/src/modules/smart-storage-unit/SmartStorageUnitLib.sol";
 import { FRONTIER_WORLD_DEPLOYMENT_NAMESPACE } from "@eveworld/common-constants/src/constants.sol";
+import { InventoryItemTableData, InventoryItemTable } from "@eveworld/world/src/codegen/tables/InventoryItemTable.sol";
+import { Utils as InventoryUtils } from "@eveworld/world/src/modules/inventory/Utils.sol";
 
-contract DepositToInventory is Script {
-  using SmartStorageUnitLib for SmartStorageUnitLib.World;
+contract WithdrawFromInventory is Script {
+  using InventoryLib for InventoryLib.World;
+  using InventoryUtils for bytes14;
 
   function run(address worldAddress) public {
     StoreSwitch.setStoreAddress(worldAddress);
@@ -20,14 +24,14 @@ contract DepositToInventory is Script {
 
     // Start broadcasting transactions from the deployer account
     vm.startBroadcast(deployerPrivateKey);
-    SmartStorageUnitLib.World memory smartStorageUnit = SmartStorageUnitLib.World({
+    InventoryLib.World memory inventory = InventoryLib.World({
       iface: IBaseWorld(worldAddress),
       namespace: FRONTIER_WORLD_DEPLOYMENT_NAMESPACE
     });
 
     uint256 smartObjectId = uint256(keccak256(abi.encode("item:<tenant_id>-<db_id>-2345")));
     InventoryItem[] memory items = new InventoryItem[](3);
-    items[0] = InventoryItem({ inventoryItemId: 123, owner: invOwner, itemId: 0, typeId: 23, volume: 10, quantity: 5 });
+    items[0] = InventoryItem({ inventoryItemId: 123, owner: invOwner, itemId: 0, typeId: 23, volume: 10, quantity: 1 });
     items[1] = InventoryItem({
       inventoryItemId: 1234,
       owner: invOwner,
@@ -42,10 +46,32 @@ contract DepositToInventory is Script {
       itemId: 0,
       typeId: 35,
       volume: 10,
-      quantity: 300
+      quantity: 10
     });
 
-    smartStorageUnit.createAndDepositItemsToInventory(smartObjectId, items);
+    inventory.withdrawFromInventory(smartObjectId, items);
+
+    InventoryItemTableData memory inventoryItem1 = InventoryItemTable.get(
+      FRONTIER_WORLD_DEPLOYMENT_NAMESPACE.inventoryItemTableId(),
+      smartObjectId,
+      items[0].inventoryItemId
+    );
+    InventoryItemTableData memory inventoryItem2 = InventoryItemTable.get(
+      FRONTIER_WORLD_DEPLOYMENT_NAMESPACE.inventoryItemTableId(),
+      smartObjectId,
+      items[1].inventoryItemId
+    );
+
+    console.log(inventoryItem1.quantity);
+    InventoryItemTableData memory inventoryItem3 = InventoryItemTable.get(
+      FRONTIER_WORLD_DEPLOYMENT_NAMESPACE.inventoryItemTableId(),
+      smartObjectId,
+      items[2].inventoryItemId
+    );
+
+    console.log(inventoryItem1.quantity);
+    console.log(inventoryItem2.quantity);
+    console.log(inventoryItem3.quantity);
 
     vm.stopBroadcast();
   }
