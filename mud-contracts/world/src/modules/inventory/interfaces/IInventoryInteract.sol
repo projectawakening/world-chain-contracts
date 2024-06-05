e347558c666df55fa9429e2a7b13c4a29e15ce61
CCP Legolas <toniya@ccpgames.com>
2024-06-05 00:38:29 +0000

    inventory interact inventoryToEphemeralTransferWithParam with paramater

--- mud-contracts/world/src/modules/inventory/interfaces/IInventoryInteract.sol
+++ mud-contracts/world/src/modules/inventory/interfaces/IInventoryInteract.sol
@@ -11,6 +11,11 @@ import { InventoryItem } from "../types.sol";
  */
 interface IInventoryInteract {
   function inventoryToEphemeralTransfer(uint256 smartObjectId, InventoryItem[] memory items) external;
+  function inventoryToEphemeralTransferWithParam(
+    uint256 smartObjectId,
+    address ephemeralInventoryOwner,
+    InventoryItem[] memory outItems
+  ) external;
 
   function ephemeralToInventoryTransfer(uint256 smartObjectId, InventoryItem[] memory items) external;
 
--- mud-contracts/world/src/modules/inventory/systems/InventoryInteract.sol
+++ mud-contracts/world/src/modules/inventory/systems/InventoryInteract.sol
@@ -88,6 +88,64 @@ contract InventoryInteract is EveSystem {
    * @notice Transfer items from ephemeral to inventory
    * @dev transfer items from ephemeral to inventory
    * @param smartObjectId is the smart object id
+   * @param ephemeralInventoryOwner is the ephemeral inventory owner
+   * @param outItems is the array of items to transfer
+   */
+  function inventoryToEphemeralTransferWithParam(
+    uint256 smartObjectId,
+    address ephemeralInventoryOwner,
+    InventoryItem[] memory outItems
+  ) public hookable(smartObjectId, _systemId()) {
+    address owner = IERC721(DeployableTokenTable.getErc721Address(_namespace().deployableTokenTableId())).ownerOf(
+      smartObjectId
+    );
+    InventoryItem[] memory inItems = new InventoryItem[](outItems.length);
+
+    for (uint i = 0; i < outItems.length; i++) {
+      InventoryItem memory item = outItems[i];
+      if (
+        InventoryItemTable.get(_namespace().inventoryItemTableId(), smartObjectId, item.inventoryItemId).quantity <
+        item.quantity
+      ) {
+        revert IInventoryErrors.Inventory_InvalidItemQuantity(
+          "InventoryInteract: Not enough items to transfer",
+          item.inventoryItemId,
+          item.quantity
+        );
+      }
+
+      //Ephemeral Inventory Owner is the address of the caller of this function to whom the items are being transferred
+      inItems[i] = InventoryItem({
+        inventoryItemId: item.inventoryItemId,
+        owner: ephemeralInventoryOwner,
+        itemId: item.itemId,
+        typeId: item.typeId,
+        volume: item.volume,
+        quantity: item.quantity
+      });
+
+      //Emitting the event before the transfer to reduce loop execution, might need to consider security implications later
+      ItemTransferOffchainTable.set(
+        _namespace().itemTransferTableId(),
+        smartObjectId,
+        item.inventoryItemId,
+        owner,
+        ephemeralInventoryOwner,
+        item.quantity,
+        block.timestamp
+      );
+    }
+
+    //withdraw the items from inventory and deposit to ephemeral table
+    _inventoryLib().withdrawFromInventory(smartObjectId, outItems);
+    //transfer the items to ephemeral owner who is the caller of this function
+    _inventoryLib().depositToEphemeralInventory(smartObjectId, ephemeralInventoryOwner, inItems);
+  }
+
+  /**
+   * @notice Transfer items from ephemeral to inventory
+   * @dev transfer items from ephemeral to inventory
+   * @param smartObjectId is the smart object id
    * @param items is the array of items to transfer
    */
   function ephemeralToInventoryTransfer(
