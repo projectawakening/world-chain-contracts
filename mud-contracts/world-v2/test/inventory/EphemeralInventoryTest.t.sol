0b628beeabac0bafc3ca2405c7e8eb4f1f6ade10
ledablack <chloe.se@ccpgames.com>
2024-08-25 01:10:38 -0700

    test: inventory test files

---
+++ mud-contracts/world-v2/test/inventory/EphemeralInventoryTest.t.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.24;
+
+import "forge-std/Test.sol";
+import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
+import { IBaseWorld } from "@latticexyz/world/src/codegen/interfaces/IBaseWorld.sol";
+
+contract SmartCharacterTest is MudTest {
+  IBaseWorld world;
+
+  function setUp() public virtual override {
+    super.setUp();
+    world = IBaseWorld(worldAddress);
+  }
+
+  function testWorldExists() public {
+    uint256 codeSize;
+    address addr = worldAddress;
+    assembly {
+      codeSize := extcodesize(addr)
+    }
+    assertTrue(codeSize > 0);
+  }
+}
