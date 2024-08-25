ee7fa055f45acc49d18eb370ed64c6b5b2fb03db
ledablack <chloe.se@ccpgames.com>
2024-08-24 19:17:21 -0700

    feat: added inventory base files

---
+++ mud-contracts/world-v2/src/systems/inventory/InventorySystem.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.24;
+
+import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
+import { EveSystem } from "../EveSystem.sol";
+
+contract InventorySystem is EveSystem {}
