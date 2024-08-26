4655718edac60eaa1c5526714c9495b01aef965e
CCP Error <error@ccpgames.com>
2024-08-26 17:38:25 +0000

    feat: kill mail
    Support for storing kill mails on chain.
    
    Resolves https://ccpgames.atlassian.net/browse/EF-5113.

---
+++ mud-contracts/world/src/modules/kill-mail/constants.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.21;
+
+bytes16 constant KILL_MAIL_SYSTEM_NAME = "KillMail";
+
+bytes16 constant KILL_MAIL_MODULE_NAME = "KillMail";
+bytes14 constant KILL_MAIL_MODULE_NAMESPACE = "KillMail";
+
+bytes16 constant KILL_MAIL_TABLE_NAME = "KillMailTable";
+
+bytes14 constant KILL_MAIL_DEPLOYMENT_NAMESPACE = "eveworld";
