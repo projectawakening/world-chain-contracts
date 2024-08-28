3143efdf48c1a9ec17b7014f8c09a7f7b884adba
ledablack <chloe.se@ccpgames.com>
2024-08-27 18:19:21 -0700

    test: inventory test contracts

---
+++ mud-contracts/world-v2/test/inventory/InventoryInteractTest.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.24;
+
+import "forge-std/Test.sol";
+import { MudTest } from "@latticexyz/world/test/MudTest.t.sol";
+import { IBaseWorld } from "@latticexyz/world/src/codegen/interfaces/IBaseWorld.sol";
+import { World } from "@latticexyz/world/src/World.sol";
+
+contract InventoryInteractTest is MudTest {
+  IBaseWorld world;
+
+  function setUp() public virtual override {
+    super.setUp();
+    world = IBaseWorld(worldAddress);
+  }
+}
