2c7dcd6c527e63ef56c25fa2c84a5b2f6d40bc5b
CCP Tezza <tezza@ccpgames.com>
2024-09-24 02:42:08 +0800

    add smart character requirements to end-to-end-test runAll

--- end-to-end-tests/script/CreateAndAnchor.s.sol
+++ end-to-end-tests/script/CreateAndAnchor.s.sol
@@ -7,16 +7,25 @@ import { ResourceId, WorldResourceIdLib } from "@latticexyz/world/src/WorldResou
 import { IBaseWorld } from "@eveworld/world/src/codegen/world/IWorld.sol";
 import { EntityRecordData, SmartObjectData, WorldPosition, Coord } from "@eveworld/world/src/modules/smart-storage-unit/types.sol";
 import { SmartStorageUnitLib } from "@eveworld/world/src/modules/smart-storage-unit/SmartStorageUnitLib.sol";
+import { SmartCharacterLib } from "@eveworld/world/src/modules/smart-character/SmartCharacterLib.sol";
+import { EntityRecordData as CharEntityRecordData } from "@eveworld/world/src/modules/smart-character/types.sol";
+import { EntityRecordOffchainTableData } from "@eveworld/world/src/codegen/tables/EntityRecordOffchainTable.sol";
 import { FRONTIER_WORLD_DEPLOYMENT_NAMESPACE } from "@eveworld/common-constants/src/constants.sol";
 
 contract CreateAndAnchor is Script {
   using SmartStorageUnitLib for SmartStorageUnitLib.World;
+  using SmartCharacterLib for SmartCharacterLib.World;
 
   function run(address worldAddress) public {
     StoreSwitch.setStoreAddress(worldAddress);
     // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
     address player = vm.addr(deployerPrivateKey);
+
+    SmartCharacterLib.World memory smartCharacter = SmartCharacterLib.World({
+      iface: IBaseWorld(worldAddress),
+      namespace: FRONTIER_WORLD_DEPLOYMENT_NAMESPACE
+    });
 
     // Start broadcasting transactions from the deployer account
     vm.startBroadcast(deployerPrivateKey);
@@ -26,7 +35,14 @@ contract CreateAndAnchor is Script {
     });
 
     uint256 smartObjectId = uint256(keccak256(abi.encode("item:<tenant_id>-<db_id>-2345")));
-
+    smartCharacter.createCharacter(
+      12512,
+      player,
+      222,
+      CharEntityRecordData({ typeId: 123, itemId: 237, volume: 100 }),
+      EntityRecordOffchainTableData({ name: "awesome character", dappURL: "noURL", description: "." }),
+      "azert"
+    );
     smartStorageUnit.createAndAnchorSmartStorageUnit(
       smartObjectId,
       EntityRecordData({ typeId: 7888, itemId: 111, volume: 10 }),
--- end-to-end-tests/script/CreateSmartCharacter.s.sol
+++ end-to-end-tests/script/CreateSmartCharacter.s.sol
@@ -22,8 +22,8 @@ contract CreateSmartCharacter is Script {
     // TODO accept as parameters to the run method for test reproducability
     uint256 characterId = 12513;
     uint256 corpId = 222;
-    // The address this character will be minted to
-    address characterAddress = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+    // The token of this character will be minted to
+    address characterAddress = vm.addr(deployerPrivateKey);
 
     uint256 typeId = 123;
     uint256 itemId = 234;
