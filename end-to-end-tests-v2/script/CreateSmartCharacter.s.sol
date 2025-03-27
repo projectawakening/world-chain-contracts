0294674d6bcc1684e941f7b1528e33a6b643fb2d
CCP Legolas <toniya@ccpgames.com>
2025-03-27 01:26:06 +0000

    integration tests for smartCharacter and anchor ssu

--- end-to-end-tests-v2/script/CreateSmartCharacter.s.sol
+++ end-to-end-tests-v2/script/CreateSmartCharacter.s.sol
@@ -4,12 +4,13 @@ import { Script } from "forge-std/Script.sol";
 import { console } from "forge-std/console.sol";
 import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
 import { ResourceId, WorldResourceIdLib } from "@latticexyz/world/src/WorldResourceId.sol";
-import { IBaseWorld } from "@latticexyz/world/src/codegen/interfaces/IBaseWorld.sol";
 
-import { EntityRecordData, EntityMetadata } from "@eveworld/world-v2/src/namespaces/evefrontier/systems/entity-record/types.sol";
-import { SmartCharacterSystem } from "@eveworld/world-v2/src/namespaces/evefrontier/systems/smart-character/SmartCharacterSystem.sol";
+import { Tenant, EntityRecordMetadata, EntityRecordMetadataData, Characters, CharactersData, CharactersByAccount } from "@eveworld/world-v2/src/namespaces/evefrontier/codegen/index.sol";
 
-import { smartCharacterSystem } from "@eveworld/world-v2/src/namespaces/evefrontier/codegen/systems/SmartCharacterSystemLib.sol";
+import { EntityRecordParams, EntityMetadataParams } from "@eveworld/world-v2/src/namespaces/evefrontier/systems/entity-record/types.sol";
+
+import { SmartCharacterSystem, smartCharacterSystem } from "@eveworld/world-v2/src/namespaces/evefrontier/systems/smart-character/SmartCharacterSystem.sol";
+import { ObjectIdLib } from "@eveworld/world-v2/src/namespaces/evefrontier/libraries/ObjectIdLib.sol";
 
 contract CreateSmartCharacter is Script {
   function run(address worldAddress) public {
@@ -17,21 +18,32 @@ contract CreateSmartCharacter is Script {
     // Load the private key from the `PRIVATE_KEY` environment variable (in .env)
     uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
     vm.startBroadcast(deployerPrivateKey);
-    IBaseWorld world = IBaseWorld(worldAddress);
 
     // Test values for creating the smart character
-    uint256 characterId = 123;
-    address characterAddress = vm.addr(deployerPrivateKey);
+    uint256 characterTypeId = vm.envUint("CHARACTER_TYPE_ID");
+    uint256 characterItemId = 1347;
+    bytes32 tenantId = Tenant.get();
+    uint256 characterSmartObjectId = ObjectIdLib.calculateSingletonId( tenantId, characterItemId);
+    address characterAddress = vm.addr(vm.envUint("PLAYER_PRIVATE_KEY"));
     uint256 tribeId = 100;
-    EntityRecordData memory entityRecord = EntityRecordData({ typeId: 123, itemId: 234, volume: 100 });
-
-    EntityMetadata memory entityRecordMetadata = EntityMetadata({
+    EntityRecordParams memory entityRecordParams = EntityRecordParams({
+      tenantId: tenantId,
+      typeId: characterTypeId,
+      itemId: characterItemId,
+      volume: 0
+    });
+    EntityMetadataParams memory entityRecordMetadataParams = EntityMetadataParams({
       name: "name",
       dappURL: "dappURL",
       description: "description"
     });
 
-    smartCharacterSystem.createCharacter(characterId, characterAddress, tribeId, entityRecord, entityRecordMetadata);
+    smartCharacterSystem.createCharacter(characterSmartObjectId, characterAddress, tribeId, entityRecordParams, entityRecordMetadataParams);
+
+    CharactersData memory character = Characters.get(characterSmartObjectId);
+    console.log("Character created:");
+
+    console.log("Characters by account:", CharactersByAccount.getSmartObjectId(characterAddress));
 
     vm.stopBroadcast();
   }
