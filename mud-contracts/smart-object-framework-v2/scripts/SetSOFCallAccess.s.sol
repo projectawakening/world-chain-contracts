a08c2603c63904df876adb1cddcb13f7e1beff5b
dk1a <dk1a@protonmail.com>
2025-05-12 15:34:45 +0300

    refactor: export sof-v2 scripts as pure functions

--- mud-contracts/smart-object-framework-v2/scripts/SetSOFCallAccess.s.sol
+++ mud-contracts/smart-object-framework-v2/scripts/SetSOFCallAccess.s.sol
@@ -28,23 +28,28 @@ contract SetSOFCallAccess is Script {
 
     vm.startBroadcast(deployerPrivateKey);
 
-    // TagSystem.sol
-    CallAccess.set(tagSystem.toResourceId(), ITagSystem.setTag.selector, entitySystem.getAddress(), true);
-    CallAccess.set(tagSystem.toResourceId(), ITagSystem.removeTag.selector, entitySystem.getAddress(), true);
+    runSetSOFCallAccess(worldAddress);
 
-    // RoleManagementSystem.sol
-    CallAccess.set(
-      roleManagementSystem.toResourceId(),
-      IRoleManagementSystem.scopedCreateRole.selector,
-      entitySystem.getAddress(),
-      true
-    );
-    CallAccess.set(
-      roleManagementSystem.toResourceId(),
-      IRoleManagementSystem.scopedRevokeAll.selector,
-      entitySystem.getAddress(),
-      true
-    );
     vm.stopBroadcast();
   }
+}
+
+function runSetSOFCallAccess(address worldAddress) {
+  // TagSystem.sol
+  CallAccess.set(tagSystem.toResourceId(), ITagSystem.setTag.selector, entitySystem.getAddress(), true);
+  CallAccess.set(tagSystem.toResourceId(), ITagSystem.removeTag.selector, entitySystem.getAddress(), true);
+
+  // RoleManagementSystem.sol
+  CallAccess.set(
+    roleManagementSystem.toResourceId(),
+    IRoleManagementSystem.scopedCreateRole.selector,
+    entitySystem.getAddress(),
+    true
+  );
+  CallAccess.set(
+    roleManagementSystem.toResourceId(),
+    IRoleManagementSystem.scopedRevokeAll.selector,
+    entitySystem.getAddress(),
+    true
+  );
 }
