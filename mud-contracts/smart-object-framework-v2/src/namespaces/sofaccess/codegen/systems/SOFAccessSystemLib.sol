a0755ff72ebf467f176275d9715f08670b6b0c55
dk1a <dk1a@protonmail.com>
2025-05-13 16:23:49 +0300

    chore: update mud for sof-v2

--- mud-contracts/smart-object-framework-v2/src/namespaces/sofaccess/codegen/systems/SOFAccessSystemLib.sol
+++ mud-contracts/smart-object-framework-v2/src/namespaces/sofaccess/codegen/systems/SOFAccessSystemLib.sol
@@ -7,6 +7,7 @@ import { SOFAccessSystem } from "../../systems/sof-access-system/SOFAccessSystem
 import { revertWithBytes } from "@latticexyz/world/src/revertWithBytes.sol";
 import { IWorldCall } from "@latticexyz/world/src/IWorldKernel.sol";
 import { SystemCall } from "@latticexyz/world/src/SystemCall.sol";
+import { WorldContextConsumerLib } from "@latticexyz/world/src/WorldContext.sol";
 import { Systems } from "@latticexyz/world/src/codegen/tables/Systems.sol";
 import { ResourceId } from "@latticexyz/store/src/ResourceId.sol";
 import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
@@ -473,7 +474,7 @@ library SOFAccessSystemLib {
   }
 
   function callAsRoot(SOFAccessSystemType self) internal view returns (RootCallWrapper memory) {
-    return RootCallWrapper(self.toResourceId(), msg.sender);
+    return RootCallWrapper(self.toResourceId(), WorldContextConsumerLib._msgSender());
   }
 
   function callAsRootFrom(SOFAccessSystemType self, address from) internal pure returns (RootCallWrapper memory) {
